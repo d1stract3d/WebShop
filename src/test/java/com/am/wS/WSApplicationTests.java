0ae70b242a27f21af56b374120b222de89bf5839
Alex McCall <61480140+d1stract3d@users.noreply.github.com>
2020-05-13 13:38:59 -0500

    Upload
    Added project files.

---
+++ src/test/java/com/am/wS/WSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.am.wS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
